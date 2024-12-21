5cde416f82efd53c5d528cbf5ef729833c7fff4b
Mohamed amine Benomar <mbenomar@c1r7p8.1337.ma>
2024-12-21 10:08:59 +0100

    done

---
+++ get_next_line.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbenomar <mbenomar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/12 13:17:50 by mbenomar          #+#    #+#             */
+/*   Updated: 2024/12/21 10:03:54 by mbenomar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *str);
+char	*ft_strdup(const char *s);
+char	*ft_strchr(const char *s, char c);
+char	*ft_strjoin(char *s1, char *s2);
+
+#endif
---
+++ get_next_line_bonus.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbenomar <mbenomar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/20 13:17:50 by mbenomar          #+#    #+#             */
+/*   Updated: 2024/12/21 10:05:58 by mbenomar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+#define GET_NEXT_LINE_BONUS_H
+
+#include <limits.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 42
+#endif
+
+char *get_next_line(int fd);
+size_t ft_strlen(const char *str);
+char *ft_strdup(const char *s);
+char *ft_strchr(const char *s, char c);
+char *ft_strjoin(char *s1, char *s2);
+
+#endif
